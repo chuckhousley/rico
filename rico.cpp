b2feefda19451f79ce2f91049cc72ff423427c8a
Chuck <charles@charleshousley.com>
2013-10-30 21:13:37 -0500

    Now reading in @data values in a not-awful way

--- rico.cpp
+++ rico.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstdlib>
 using namespace std;
 
 struct attributes{
@@ -10,41 +11,51 @@ struct attributes{
 };
 
 int main() {
-	//one last last test
     string filename;
     string line;
     string relation;
+    
     vector<attributes> attr;
-    ifstream in;
     vector< vector<string> > data;
-    cout<<"Enter a filename: "<<endl;
-    cin>>filename;
+    
+    cout<<"Enter a filename: ";
+    //cin>>filename;
+    filename = "b";
+    
+    ifstream in;
     in.open(filename.c_str());
-    if(in.is_open()) {
-        while (getline(in, line)) {
-            if (line.substr(0,2) == "@a") {
-                attributes new_attr;
-                cout << line << endl;
-                size_t itr1 = line.find(' ');
-                size_t itr2 = line.find(' ', itr1+1);
-                new_attr.name = line.substr(itr1+1, itr2-itr1-1);
-                new_attr.type = line.substr(itr2+1);
-                attr.push_back(new_attr);
-            }
-            if (line.substr(0, 1) != "@" && line.substr(0, 1) != "\n") {
-                vector<string> row;
-                while(line.substr(0,1) != "/n") {
-                    size_t itr1 = line.find(',');
-                    cout << line.subs
-                    /*row.push_back(line.substr(0,itr1));
-                    line.erase(0, itr1);*/
+    
+    if(!in.is_open()) {
+        cout << "Failed to open file" << endl;
+        exit(2);
+    }
+    while (getline(in, line)) {
+        //Reading in the attributes
+        if (line.substr(0,2) == "@a") {
+            attributes new_attr;
+            size_t itr1 = line.find(' ');
+            size_t itr2 = line.find(' ', itr1+1);
+            new_attr.name = line.substr(itr1+1, itr2-itr1-1);
+            new_attr.type = line.substr(itr2+1);
+            attr.push_back(new_attr);
+        }
+        //reading in the data
+        //assumes that data does not contain commas
+        //commas are used for the delimiter only
+        if (line.substr(0, 1) != "@" && line.find(",") != string::npos) {
+            vector<string> row;
+            string entry = "";
+            for (int i = 0; i < line.size(); i++) {
+                if (line[i] != ',') {
+                    entry += line[i];
                 }
-                for (int i = 0; i < row.size(); i++)
-                    cout<< row[i] << ',';
-                cout << endl;
-                data.push_back(row);
+                else {
+                    row.push_back(entry);
+                    entry = "";
+                }
             }
-
+            row.push_back(entry);
+            data.push_back(row);
         }
     }
     in.close();
