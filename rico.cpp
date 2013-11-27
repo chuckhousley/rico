1825b9e3eda16dfc30b9d1768b382888300ab1ba
Chuck <charles@charleshousley.com>
2013-11-26 20:00:30 -0600

    Cleaned up inputs, new output, wrote in commented code for checking for duplicate entries

--- rico.cpp
+++ rico.cpp
@@ -25,9 +25,10 @@ int main() {
     vector< vector<string> > data;
     vector<coverings> cover;
 	
+    // Gets the name of the input file
     cout<<"Enter a filename: ";
     //cin>>filename;
-    filename = "table3_10_fg.arff";
+    filename = "table3_10_fg.arff"; //TESTING PURPOSES ONLY
     
     ifstream in;
     in.open(filename.c_str());
@@ -36,6 +37,7 @@ int main() {
         cout << "Failed to open file" << endl;
         exit(2);
     }
+    
 	//Parses ARFF file 
     while (getline(in, line)) {
         //Reading in the attributes
@@ -44,7 +46,7 @@ int main() {
             size_t itr1 = line.find(' ');
             size_t itr2 = line.find(' ', itr1+1);
             new_attr.name = line.substr(itr1+1, itr2-itr1-1);
-            new_attr.type = line.substr(itr2+1);
+            new_attr.type = line.substr(itr2+1); //TODO: clean up type input
             attr.push_back(new_attr);
         }
         //reading in the data
@@ -66,25 +68,67 @@ int main() {
             data.push_back(row);
         }
     }
-	cout << "List of Attributes:" << endl;
+    in.close();
+    
+	cout << "\nList of Attributes:" << endl;
 	for( unsigned int i = 0; i < attr.size(); i++)
 	{
 	  cout << i << ". " << attr[i].name << endl;
 	}
+    
 	unsigned int num = 0;
-	cout << "Please Enter How Many Decision Attributes You Would Like" << endl;
-	// Add a check 
-	//cin >> num;
-	num = 1;
-	vector<unsigned int> decAttr(num,-1);
-	cout << "Please Enter the Numbers of All Decision Attributes" << endl;
+	cout << "\nPlease enter how many decision attributes you would like: ";
+    string num_input;
+	cin >> num_input;
+    num = atoi(num_input.c_str());
+    
+    while (num == 0 || num > attr.size()) {
+        cout << "Invalid input, please enter how many decision attributes you would like: ";
+        cin >> num_input;
+        num = atoi(num_input.c_str());
+    }
+	num = 2; //TESTING PURPOSES ONLY
+    
+    
+	vector<unsigned int> decAttr(num,-1); 
+	cout << "Please enter the numbers of all decision attributes: " << endl;
 	for(unsigned int i = 0; i < num; i++)
 	{
-	  //cin >> decAttr[i];
+        /*cout << "Attribute " << i << ": ";
+        bool duplicate = true;
+        while (duplicate) {
+            duplicate = false;
+            string dec_input;
+            cin >> dec_input;
+            
+            int dec_input_i = atoi(dec_input.c_str());
+            while (dec_input_i < 1) {
+                cout << "Invalid input, try again: ";
+                cin >> dec_input;
+                dec_input_i = atoi(dec_input.c_str());
+            }
+            
+            for (int j = 0; j < i; j++) {
+                if (dec_input_i == decAttr[j]) {
+                    duplicate = true;
+                }
+            }
+            
+            if (!duplicate) {
+                decAttr[i] = dec_input_i;
+                break;
+            }
+            else {
+                cout << Duplicate number detected, please choose another: ";
+            }
+        }*/
 	}
-	decAttr[0] = 5;
-	cout << "Please Enter the Maxium # of attributes to cosider a covering" << endl; 
-	cin >> maxAttr; 
+    
+	decAttr[0] = 5; //TESTING PURPOSES
+    decAttr[1] = 4;
+	cout << "Please enter the maxium # of attributes to be considered as a covering: ";
+	cin >> maxAttr;
+    
 	// Generates Coverings single attr
 	for( unsigned int i = 0; i < attr.size(); i++)
 	{
@@ -105,18 +149,38 @@ int main() {
 	  }
 	  cover.push_back(newCover);
 	}
+    
 	// coverings to size maxAttr
 	
 	//rico
 	
 	//After Running rico 
-	cout << "Please Enter the Minimum Coverage required for a rule" << endl;
+	cout << "Please enter the minimum coverage required for a rule: ";
 	cin >> minCoverage;
+    
+    string unnecessary_input;
+    cout << "Should unnecessary conditions be dropped? (y/n): ";
+    cin >> unnecessary_input;
+    
+    while (unnecessary_input != "y" && unnecessary_input != "n") {
+        cout << "Please enter 'y' or 'n': ";
+        cin >> unnecessary_input;
+    }
+    
+    bool unnecessary_dropped = ((unnecessary_input == "y")? true: false);
 	
 	//Reporting 
-	
-	
-	
-    in.close();
+    cout << "\n----------OUTPUT----------\n";
+    cout << "Name of input file: " << filename << endl;
+    cout << "Decision Attributes: ";
+    for (int i = 0; i < decAttr.size(); i++) {
+        cout << attr[decAttr[i]].name;
+        cout << ((i < decAttr.size()-1)? ", ":"");
+    }
+    cout << "\nMaximum number of attributes to be considered for a covering: " << maxAttr;
+    cout << "\nMinimum coverage required for a rule to be reported: " << minCoverage;
+    cout << "\nUnnecessary conditions have been " << ((unnecessary_dropped)? "dropped.": "left in.") << endl;
+    
+    cout << endl; //one more empty line for the hell of it, and we're done
     return 0;
 }
