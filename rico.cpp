8460df5d9d4286becc1e0a01d59444c5f5fa2a3f
Chuck <charles@charleshousley.com>
2013-12-04 21:30:29 -0600

    merged changes

--- rico.cpp
+++ rico.cpp
@@ -8,7 +8,6 @@ Presented by Computer Science 301
 #include <string>
 #include <vector>
 #include <cstdlib>
-
 #include <algorithm>
 using namespace std;
 
@@ -17,11 +16,9 @@ struct attributes{
     string type;
 };
 
-<<<<<<< .merge_file_a05032
-struct rule {
-    vector<int> attributes;
-    string conditions;
-    int occurances;
+struct rule{
+    vector <string> data; 
+    int instances;
 };
 
 struct covering{
@@ -29,74 +26,33 @@ struct covering{
     vector< vector<int> > partitions;
 };
 
-unsigned int factorial(unsigned int n);
 bool comparePartitions(vector< vector<int> > p1, vector< vector<int> > p2);
 vector< vector<unsigned int> > generateCombinations(vector<attributes> attr, vector<unsigned int> decAttr, unsigned int maxAttr);
-=======
-struct covering{
-     vector<unsigned int> attributes;
-    vector< vector<int> > partitions;
-};
-
-struct rule{
-vector <string> data; 
-int instances;
-};
-
 void Parser( vector<attributes> &attr, vector< vector<string> > &data);
 void listAttributes( vector < attributes > attr);
 void Report( string filename, int maxAttr, int minCoverage, bool unnecessary_dropped, vector <attributes> attr, vector <unsigned int> decAttr);
-void rico( vector <covering> cover, vector < vector < string> > data, vector<unsigned int> decAttr);
->>>>>>> .merge_file_a03220
+void rico(vector<covering> cover, vector< vector<string> > data, vector<unsigned int> decAttr);
 
 int main() {
     string filename = "I need to fix this";
 	string num_input;
 	string maxAttr_input;
     unsigned int maxAttr = 0, minCoverage = 1;
+
     unsigned int num = 0;
 	
     vector<attributes> attr;
     vector< vector<string> > data;
-<<<<<<< .merge_file_a05032
     vector<covering> cover;
     vector< vector<unsigned int> > combinations;
-=======
-    vector<covering> cover(1);
-	vector <int> c0;
-	c0.push_back(0);
-	c0.push_back(2);
-    cover[0].partitions.push_back(c0);
-	vector <int> c1;
-	c0.push_back(1);
-	c0.push_back(3);
-    cover[0].partitions.push_back(c1);
-	vector <int> c2;
-	c0.push_back(4);
-	c0.push_back(5);
-    cover[0].partitions.push_back(c2);
-	vector <int> c3;
-	c0.push_back(6);
-	c0.push_back(7);
-    cover[0].partitions.push_back(c3);
-	
-	cover[0].attributes.push_back(0);
-	cover[0].attributes.push_back(2);
-	
-	
-	// Does the File IO things 
+    // Does the File IO things 
 	// and parsers the Input
 	Parser( attr, data);
    
     // Lists Attributes
-    listAttributes( attr);   
->>>>>>> .merge_file_a03220
-    
+    listAttributes( attr);  
     cout << "\nPlease enter how many decision attributes you would like: ";
-<<<<<<< .merge_file_a05032
     /*string num_input;
-=======
->>>>>>> .merge_file_a03220
     cin >> num_input;
     num = atoi(num_input.c_str());
     
@@ -106,13 +62,8 @@ int main() {
         num = atoi(num_input.c_str());
     }*/
     num = 1; //TESTING PURPOSES ONLY
-<<<<<<< .merge_file_a05032
     
     vector<unsigned int> decAttr(num,-1);
-=======
-
-	vector<unsigned int> decAttr(num,-1);
->>>>>>> .merge_file_a03220
     cout << "Please enter the numbers of all decision attributes: " << endl;
     /*for(unsigned int i = 0; i < num; i++)
     {
@@ -144,13 +95,7 @@ int main() {
     }*/
     decAttr[0] = 4; //TESTING PURPOSES
 
-<<<<<<< .merge_file_a05032
-    string maxAttr_input;
-=======
-    decAttr[0] = 5; //TESTING PURPOSES
-    //decAttr[1] = 4;
-    
->>>>>>> .merge_file_a03220
+
     cout << "Please enter the maxium # of attributes to be considered as a covering: ";
     /*cin >> maxAttr_input;
     maxAttr = atoi(maxAttr_input.c_str());
@@ -195,7 +140,6 @@ int main() {
         }
         cout << "\n";
     }
-<<<<<<< .merge_file_a05032
     
     //create decision base covering (not actually a covering, but what other coverings are based on)
     covering decision;
@@ -340,16 +284,10 @@ int main() {
     
     
 
-=======
-
-    // Generates Coverings single attr
-    
->>>>>>> .merge_file_a03220
     // coverings to size maxAttr
     
     //rico
-    rico( cover, data, decAttr);
-	
+    //rico(cover, data, decAttr);
     //After Running rico
 
 
@@ -365,97 +303,8 @@ int main() {
     
     bool unnecessary_dropped = ((unnecessary_input == "y")? true: false);
         
-<<<<<<< .merge_file_a05032
-    //Reporting
-=======
-    //Prints the Report
+  //Prints the Report
 	Report( filename, maxAttr, minCoverage, unnecessary_dropped, attr, decAttr);
-    return 0;
-}
-
-
-// Funtion that does File IO and Pasering stuff
-void Parser( vector<attributes> &attr, vector< vector<string> > &data)
-{
-    string filename;
-    string line;
-    string relation;
-
-	// Gets the name of the input file
-    cout<<"Enter a filename: ";
-    //cin>>filename;
-    filename = "table3_10_fg.arff"; //TESTING PURPOSES ONLY
-    
-    ifstream in;
-    in.open(filename.c_str());
-    
-    if(!in.is_open()) {
-        cout << "Failed to open file" << endl;
-        exit(2);
-    }
-    
-    //Parses ARFF file
-    while (getline(in, line)) {
-        //Reading in the attributes
-        if (line.substr(0,2) == "@a") {
-            attributes new_attr;
-            size_t itr1 = line.find(' ');
-            size_t itr2 = line.find(' ', itr1+1);
-            new_attr.name = line.substr(itr1+1, itr2-itr1-1);
-            new_attr.type = line.substr(itr2+1); //TODO: clean up type input
-            attr.push_back(new_attr);
-        }
-        //reading in the data
-        //assumes that data does not contain commas
-        //commas are used for the delimiter only
-        if (line.substr(0, 1) != "@" && line.find(",") != string::npos) {
-            vector<string> row;
-            string entry = "";
-            for (int i = 0; i < line.size(); i++) {
-                if (line[i] != ',') {
-                    entry += line[i];
-                }
-                else {
-                    row.push_back(entry);
-                    entry = "";
-                }
-            }
-            row.push_back(entry);
-            data.push_back(row);
-        }
-    }
-    in.close();
-}
-
-
-// Prints a list of each name in the vector of attributes attr
-void listAttributes( vector < attributes > attr)
-{
-	cout << "\nList of Attributes:" << endl;
-    for( unsigned int i = 0; i < attr.size(); i++)
-    {
-        cout << i << ". " << attr[i].name << endl;
-    }
-}
-
-// Prints out the final results 
-void Report( string filename, int maxAttr, int minCoverage, bool unnecessary_dropped, vector <attributes> attr, vector<unsigned int> decAttr)
-{
->>>>>>> .merge_file_a03220
-    cout << "\n----------OUTPUT----------\n";
-    cout << "Name of input file: " << filename << endl;
-    cout << "\nMaximum number of attributes to be considered for a covering: " << maxAttr;
-    cout << "\nMinimum coverage required for a rule to be reported: " << minCoverage;
-    cout << "\nUnnecessary conditions have been " << ((unnecessary_dropped)? "dropped.\n": "left in.\n") << endl;
-    
-    cout << "Decision Attribute" << ((decAttr.size() > 1)? "s: [": ": [");
-    for (int i = 0; i < decAttr.size(); i++) {
-        cout << attr[decAttr[i]].name;
-        cout << ((i < decAttr.size()-1)? ", ":"]\n");
-    }
-   
-    cout << endl; //one more empty line for the hell of it, and we're done
-<<<<<<< .merge_file_a05032
     return 0;
 }
 
@@ -501,13 +350,6 @@ vector< vector<unsigned int> > generateCombinations(vector<attributes> attr, vec
 }
 
 
-unsigned int factorial(unsigned int n) 
-{
-    if (n == 0)
-       return 1;
-    return n * factorial(n - 1);
-}
-
 
 bool comparePartitions(vector< vector<int> > p1, vector< vector<int> > p2) {
     if(p1.size() != p2.size()) {
@@ -526,10 +368,106 @@ bool comparePartitions(vector< vector<int> > p1, vector< vector<int> > p2) {
         }
     }
     return true;
-=======
 }
 
-void rico( vector <covering> cover, vector < vector < string> > data, vector<unsigned int> decAttr)
+
+// Funtion that does File IO and Pasering stuff
+void Parser( vector<attributes> &attr, vector< vector<string> > &data)
+{
+    string filename;
+    string line;
+    string relation;
+
+	// Gets the name of the input file
+    cout<<"Enter a filename: ";
+    //cin>>filename;
+    filename = "table3_10_fg.arff"; //TESTING PURPOSES ONLY
+    
+    ifstream in;
+    in.open(filename.c_str());
+    
+    if(!in.is_open()) {
+        cout << "Failed to open file" << endl;
+        exit(2);
+    }
+
+
+    //Parses ARFF file
+    while (getline(in, line)) {
+        //Reading in the attributes
+        if (line.substr(0,2) == "@a") {
+            attributes new_attr;
+            size_t itr1 = line.find(' ');
+            size_t itr2 = line.find(' ', itr1+1);
+            new_attr.name = line.substr(itr1+1, itr2-itr1-1);
+            new_attr.type = line.substr(itr2+1); //TODO: clean up type input
+            attr.push_back(new_attr);
+        }
+        //reading in the data
+        //assumes that data does not contain commas
+        //commas are used for the delimiter only
+        if (line.substr(0, 1) != "@" && line.find(",") != string::npos) {
+            vector<string> row;
+            string entry = "";
+            for (int i = 0; i < line.size(); i++) {
+                if (line[i] != ',') {
+                    entry += line[i];
+                }
+                else {
+                    row.push_back(entry);
+                    entry = "";
+                }
+            }
+
+
+            row.push_back(entry);
+            data.push_back(row);
+        }
+
+
+
+    }
+
+
+    in.close();
+}
+
+
+
+// Prints a list of each name in the vector of attributes attr
+void listAttributes( vector < attributes > attr)
+{
+
+
+
+	cout << "\nList of Attributes:" << endl;
+    for( unsigned int i = 0; i < attr.size(); i++)
+    {
+        cout << i << ". " << attr[i].name << endl;
+    }
+}
+
+// Prints out the final results 
+void Report( string filename, int maxAttr, int minCoverage, bool unnecessary_dropped, vector <attributes> attr, vector<unsigned int> decAttr)
+{
+    cout << "\n----------OUTPUT----------\n";
+    cout << "Name of input file: " << filename << endl;
+    cout << "\nMaximum number of attributes to be considered for a covering: " << maxAttr;
+    cout << "\nMinimum coverage required for a rule to be reported: " << minCoverage;
+    cout << "\nUnnecessary conditions have been " << ((unnecessary_dropped)? "dropped.\n": "left in.\n") << endl;
+    
+
+    cout << "Decision Attribute" << ((decAttr.size() > 1)? "s: [": ": [");
+    for (int i = 0; i < decAttr.size(); i++) {
+        cout << attr[decAttr[i]].name;
+        cout << ((i < decAttr.size()-1)? ", ":"]\n");
+    }
+
+   
+    cout << endl; //one more empty line for the hell of it, and we're done
+}
+
+void rico(vector<covering> cover, vector< vector<string> > data, vector<unsigned int> decAttr)
 {
   unsigned int input = 0;
   cout << "Please Choose a Covering:" << endl;
@@ -565,8 +503,7 @@ void rico( vector <covering> cover, vector < vector < string> > data, vector<uns
 	cout << "]";
 	cout << rules[i].instances << "]" << endl;
   }
-  cout << "]" << endl;
-  /*
+    /*
   for ( unsigned int i = 0; i < rules.size(); i++)
   {
     for( unsigned int j = i+1; j < rules.size(); j++)
@@ -579,5 +516,4 @@ void rico( vector <covering> cover, vector < vector < string> > data, vector<uns
 	}
   }
   }*/
->>>>>>> .merge_file_a03220
 }
